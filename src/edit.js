fddede8ddf4c78b058baad595c632274e296c95d
retrofox <rdsuarez@gmail.com>
2021-07-26 11:07:05 -0300

    add Level attribute

--- src/edit.js
+++ src/edit.js
@@ -8,7 +8,7 @@ import QRCode from 'qrcode.react';
  */
 import { __ } from '@wordpress/i18n';
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
-import { Panel, PanelBody, TextareaControl } from '@wordpress/components';
+import { Panel, PanelBody, TextareaControl, CustomSelectControl } from '@wordpress/components';
 import { Fragment } from '@wordpress/element';
 import { getButtonSizeBySlug } from '@media-manager/block-editor-complements';
 
@@ -17,8 +17,38 @@ import { getButtonSizeBySlug } from '@media-manager/block-editor-complements';
  */
 import './editor.scss';
 
+const defaultLevels = [
+	{
+		name: __( 'Level L', 'qr-block' ),
+		slug: 'L',
+		key: 'key-level-l',
+	},
+	{
+		name: __( 'Level M', 'qr-block' ),
+		slug: 'M',
+		key: 'key-level-m',
+	},
+	{
+		name: __( 'Level Q', 'qr-block' ),
+		slug: 'Q',
+		key: 'key-level-q',
+	},
+	{
+		name: __( 'Level H', 'qr-block' ),
+		slug: 'H',
+		key: 'key-level-h',
+	},
+];
+
 export default function QRBlockEdit( { attributes, setAttributes, backgroundColor, codeColor } ) {
-	const { value, size } = attributes;
+	const { value, size, level } = attributes;
+
+	function setLevel( { selectedItem } ) {
+		if ( ! selectedItem?.slug ) {
+			return;
+		}
+		setAttributes( { level: selectedItem.slug.toUpperCase() } );
+	}
 
 	return (
 		<Fragment>
@@ -31,6 +61,15 @@ export default function QRBlockEdit( { attributes, setAttributes, backgroundColo
 							onChange={ value => setAttributes( { value } ) }
 							multiple={ true }
 						/>
+
+						<CustomSelectControl
+							label={ __( 'Level', 'qr-block' ) }
+							options={ defaultLevels }
+							onChange={ setLevel }
+							value={ defaultLevels.find(
+								( option ) => option.slug === level
+							) }
+						/>
 					</PanelBody>
 				</Panel>
 			</InspectorControls>
@@ -39,6 +78,7 @@ export default function QRBlockEdit( { attributes, setAttributes, backgroundColo
 				<QRCode
 					value={ value }
 					size={ getButtonSizeBySlug( size ) * 128 - 64 }
+					level={ level }
 					fgColor={ backgroundColor?.color }
 					bgColor={ codeColor.color }
 				/>
