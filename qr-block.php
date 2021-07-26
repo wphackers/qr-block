66edac415f7ce6d4df80f22b4caa35044a9ea16b
retrofox <rdsuarez@gmail.com>
2021-07-26 00:34:32 -0300

    organizing

--- qr-block.php
+++ qr-block.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:       QR Block
- * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Description:       Create a QR code on your website with this nice block.
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.0.1
  * Author:            retrofox
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -15,12 +15,8 @@
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
  */
-function create_block_qr_block_block_init() {
+function fbqr_create_block_qr_block_block_init() {
 	register_block_type( __DIR__ );
 }
-add_action( 'init', 'create_block_qr_block_block_init' );
+add_action( 'init', 'fbqr_create_block_qr_block_block_init' );
