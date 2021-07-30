57fcbaca17df6af4fb2ee84163c1559272fe2ea6
retrofox <rdsuarez@gmail.com>
2021-07-30 17:33:36 -0300

    rendering SVG code

--- src/save.js
+++ src/save.js
@@ -1,13 +1,34 @@
+
 /**
+ * External dependencies
+ */
+import QRCode from 'qrcode.react';
+ 
+ /**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 
-export default function save() {
+export default function save( { attributes } ) {
+	const {
+		value,
+		size,
+		level,
+		codeHEXColor,
+		bgHEXColor,
+	} = attributes;
+
 	return (
-		<p { ...useBlockProps.save() }>
-			{ __( 'Qr Block – hello from the saved content!', 'qr-block' ) }
-		</p>
+		<figure { ...useBlockProps.save() }>
+			<QRCode
+				value={ value }
+				size={ size * 100 }
+				level={ level }
+				fgColor={ codeHEXColor }
+				bgColor={ bgHEXColor }
+				renderAs="svg"
+			/>
+		</figure>
 	);
 }
