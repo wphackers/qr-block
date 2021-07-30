ba99b86ff3d2b6b91c083408b6204d47cd293315
retrofox <rdsuarez@gmail.com>
2021-07-30 20:19:08 -0300

    bump 0.0.3

--- qr-block.php
+++ qr-block.php
@@ -4,7 +4,7 @@
  * Description:       Create a QR code on your website with this nice block.
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           0.0.2
+ * Version:           0.0.3
  * Author:            retrofox
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
