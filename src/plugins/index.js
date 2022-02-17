d3f7e26257b8019726935c98383f772d5c6c1d23
retrofox <rdsuarez@gmail.com>
2022-02-17 20:06:32 -0300

    generate `Visit {link}` qr code

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,13 @@
 /**
  * External dependencies
  */
+import QRCode from 'qrcode.react';
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
-import { __ } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
+import { __, sprintf } from '@wordpress/i18n';
+import { store as editorStore } from '@wordpress/editor';
+
 
 /**
  * Internal dependencies
@@ -12,15 +16,34 @@ import { QRIcon } from '../icons';
 import './editor.scss';
 
 // Post QR Code
-const PluginDocumentSettingQRCode = () => (
-    <PluginDocumentSettingPanel
-        name="post-qr-code"
-        title={ <><QRIcon /> { __( 'QR Code', 'qr-block' ) }</> }
-        className="post-qr-code"
-    >
-        Custom Panel Contents
-    </PluginDocumentSettingPanel>
-);
+const PluginDocumentSettingQRCode = () => {
+	const { link } = useSelect(
+		select => select( editorStore ).getCurrentPost(),
+		[]
+	);
+
+	const qrContent = sprintf(
+		/* translators: %s: Post permalink */
+		__( 'Vist %s', 'qr-block' ), link
+	);
+
+	return (
+		<PluginDocumentSettingPanel
+			name="post-qr-code"
+			title={ <><QRIcon /> { __( 'QR Code', 'qr-block' ) }</> }
+			className="post-qr-code"
+		>
+			<div className='post-qr-code__container'>
+				<QRCode
+					value={ qrContent }
+					size={ 200 }
+					level="L"
+					renderAs="canvas"
+				/>
+			</div>
+		</PluginDocumentSettingPanel>
+	);
+};
  
 registerPlugin( 'plugin-document-setting-qr-code-panel', {
     render: PluginDocumentSettingQRCode,
